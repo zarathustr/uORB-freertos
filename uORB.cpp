686b2d5d1bfaae7a6b6b2580cd0bc868f8a0a108
Jin Wu <jin_wu_uestc@hotmail.com>
2017-12-11 20:02:57 +0800

    Update uORB.cpp

--- uORB.cpp
+++ uORB.cpp
@@ -32,7 +32,9 @@
  ****************************************************************************/
 
 
-/* Author: Jin Wu, Shuangqi Mo
+/* @Author 1: Jin Wu
+   @Author 2: Shuangqi Mo
+   
    E-mail: jin_wu_uestc@hotmail.com
    Website: www.jinwu.science
             www.amovauto.com
